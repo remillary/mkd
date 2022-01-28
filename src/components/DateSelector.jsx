e90b841eee9b478d4580dfcf0d461ea8b837b498
lukyanov <j4g3r113@gmail.com>
2022-01-29 03:10:39 +0700

    datepicker improvements

--- src/components/DateSelector.jsx
+++ src/components/DateSelector.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import DatePicker from 'react-datepicker';
+import DatePicker, {registerLocale} from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.min.css'
 import {inject, observer} from "mobx-react";
 import {CalendarButton} from "./CalendarButton";
 import {actions} from "../lib/store";
 import moment from "moment";
+import ru from 'date-fns/locale/ru'
 
 const DateSelector = inject('store')(observer((store) => {
   const {date} = store;
@@ -13,11 +14,15 @@ const DateSelector = inject('store')(observer((store) => {
     actions.setDate(moment(date));
   }
 
+  registerLocale('ru-RU', ru);
+
   return (
     <div className="calendar">
       <DatePicker
+        locale={'ru-RU'}
         selected={date}
         onChange={onChange}
+        minDate={moment().toDate()}
         customInput={<CalendarButton/>}
       />
     </div>
